e3283a91c7e3b590e5135138407af50ec6765a65
Nagy Sándor <nagycsol@gmail.com>
2013-10-02 13:30:29 +0200

    JavaFX Arc test

--- src/GraphAlgorithms/src/hu/elte/graphalgorithms/GraphAlgorithms.java
+++ src/GraphAlgorithms/src/hu/elte/graphalgorithms/GraphAlgorithms.java
@@ -6,8 +6,10 @@ package hu.elte.graphalgorithms;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.shape.Polyline;
 import javafx.stage.Stage;
 
 /**
@@ -18,12 +20,31 @@ public class GraphAlgorithms extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
+//        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+//        
+//        Scene scene = new Scene(root);
+//        
+//        stage.setScene(scene);
+//        stage.show();
+         Group root = new Group();
+    Scene scene = new Scene(root, 260, 80);
+    stage.setScene(scene);
+
+    Group g = new Group();
+
+    Polyline polyline = new Polyline();
+    polyline.getPoints().addAll(new Double[]{
+        100.0, 100.0,
+        110.0, 110.0,
+        100.0, 100.0,
+        110.0, 90.0,
+        100.0, 100.0,
+        200.0, 100.0});
+    
+    g.getChildren().add(polyline);
+    
+    scene.setRoot(g);
+    stage.show();
     }
 
     /**
